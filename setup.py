a6bf75858a16167d3e303896921a437cc55793cc
Tamas Spisak <spisak.tms@gmail.com>
2021-07-20 17:12:59 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='mlconfound',
+     version='0.1',
+     scripts=[],
+     author="Tamas Spisak",
+     author_email="tamas.spisak@uk-essen.de",
+     description="Tools for analyzing and quanifying effects of counfounder variables "
+                 "on machine learning model predictions.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/pni-lab/mlconfound",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+         "Operating System :: OS Independent",
+     ],
+ )
