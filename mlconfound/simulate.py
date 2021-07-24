bcabfa534dd8251d27d46d5c21c9071cf0250df2
Tamas Spisak <spisak.tms@gmail.com>
2021-07-24 19:56:22 +0200

    simulations patched

--- mlconfound/simulate.py
+++ mlconfound/simulate.py
@@ -1,25 +1,14 @@
 import numpy as np
 
 
-def simulate_y_c_yhat(ts_ratio_y, cs_ratio_y,
-                      ts_ratio_c, cs_ratio_c,
-                      ts_ratio_yhat, cs_ratio_yhat,
-                      n=100, random_state=None):
+def simulate_y_c_yhat(cov_y_c,
+                      y_ratio_yhat, c_ratio_yhat,
+                      n, random_state=None):
     rng = np.random.default_rng(random_state)
-    # true signal
-    ts = rng.normal(0, 1, n)
 
-    # conf signal
-    cs = rng.normal(0, 1, n)
+    y, c = rng.multivariate_normal([0, 0], [[1, cov_y_c], [cov_y_c, 1]], n).T
 
-    # c = cs+noise
-    c = ts_ratio_c * ts + cs_ratio_c * cs + (1 - ts_ratio_c - cs_ratio_c) * rng.normal(0, 1, n)
-
-    # y = ts+cs+noise
-    y = ts_ratio_y * ts + cs_ratio_y * cs + (1 - ts_ratio_y - cs_ratio_y) * rng.normal(0, 1, n)
-
-    # yhat = cs+ts+noise
-    yhat = ts_ratio_yhat * ts + cs_ratio_yhat * cs + (1 - ts_ratio_yhat - cs_ratio_yhat) * rng.normal(0, 1, n)
+    yhat = y_ratio_yhat * y + c_ratio_yhat * c  +  (1 - y_ratio_yhat - c_ratio_yhat) * rng.normal(0, 1, n)
 
     return y, c, yhat
 
@@ -30,31 +19,22 @@ def _create_covariance_matrix(rho, p):
     return rho ** abs(a - b)
 
 
-def simulate_y_c_X(ts_ratio_y, cs_ratio_y,
-                   ts_ratio_c, cs_ratio_c,
-                   ts_ratio_X, cs_ratio_X,
+def simulate_y_c_X(cov_y_c,
+                   y_ratio_X, c_ratio_X,
                    n_features, X_corr,
-                   dirichlet_sparsity=1,
-                   n=100, random_state=None):
-
+                   dirichlet_sparsity,
+                   n, random_state=None):
     rng = np.random.default_rng(random_state)
-    # true signal
 
-    ts = rng.normal(0, 1, n)
+    y, c = rng.multivariate_normal([0, 0], [[1, cov_y_c], [cov_y_c, 1]], n).T
 
-    # conf signal
-    cs = rng.normal(0, 1, n)
+    cov_X = _create_covariance_matrix(X_corr, n_features)
 
-    # c = cs+noise
-    c = ts_ratio_c * ts + cs_ratio_c * cs + (1 - ts_ratio_c - cs_ratio_c) * rng.normal(0, 1, n)
+    signs = rng.binomial(1, 0.5, n_features) * 2 - 1
 
-    # y = ts+cs+noise
-    y = ts_ratio_y * ts + cs_ratio_y * cs + (1 - ts_ratio_y - cs_ratio_y) * rng.normal(0, 1, n)
-
-    cov = _create_covariance_matrix(X_corr, n_features)
-    X = ts_ratio_X * ts * \
-        (rng.dirichlet([dirichlet_sparsity] * n_features, 1) * (rng.binomial(1, 0.5, n_features)*2-1)).T
-    X += cs_ratio_X * cs * \
-        (rng.dirichlet([dirichlet_sparsity] * n_features, 1) * (rng.binomial(1, 0.5, n_features)*2-1)).T
-    X += (1 - ts_ratio_X - cs_ratio_X) * rng.multivariate_normal([0] * n_features, cov, n).T
+    X = y_ratio_X * y * \
+        (rng.dirichlet([dirichlet_sparsity] * n_features, 1) * np.sqrt(n_features) * signs).T
+    X += c_ratio_X * c * \
+         (rng.dirichlet([dirichlet_sparsity] * n_features, 1) * np.sqrt(n_features) * signs).T
+    X += (1 - y_ratio_X - c_ratio_X) * rng.multivariate_normal([0] * n_features, cov_X, n).T
     return y, c, X.T
--- validation/run_simulation.sh
+++ validation/run_simulation.sh
@@ -6,4 +6,4 @@ SCRIPT_PATH=$(dirname `which $0`)
 
 FILE=data_out/validation_H0.csv
 $SCRIPT_PATH/simulation.py --head > $FILE
-$SCRIPT_PATH/simulation.py -r 100 --random-seed 1 >> $FILE
+$SCRIPT_PATH/simulation.py -r 1000 --random-seed 1 >> $FILE
