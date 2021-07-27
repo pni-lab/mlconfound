291b86bd671ed05478b433fa0e9afc238437f989
Tamas Spisak <spisak.tms@gmail.com>
2021-07-27 20:51:39 +0200

    v0.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='mlconfound',
-    version='0.4',
+    version='0.5',
     packages=setuptools.find_packages(),
     scripts=[],
     author="Tamas Spisak",
