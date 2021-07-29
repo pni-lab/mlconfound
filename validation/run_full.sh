c9cd07cf4151ceecc568759c03b1d9ea6c791633
Tamas Spisak <spisak.tms@gmail.com>
2021-07-29 11:36:58 +0200

    patched for HCP

--- validation/run_full.sh
+++ validation/run_full.sh
@@ -4,16 +4,16 @@ DIR=`dirname $0`
 
 ####################### all cont
 echo "normal y, yhat, c"
-#python3 $DIR/simulation.py --out_prefix=ccc_full_normal --mode=full
+python3 $DIR/simulation.py --out_prefix=ccc_full_normal --mode=full
 
 echo "non-nomral y,c,yhat: delta=1, epsilon=1 (kurt: 1.1 skew: -1.1)"
-#python3 $DIR/simulation.py --out_prefix=ccc_full_non-normal-all --delta-yc=1 --epsilon-yc=1 --delta-yhat=1 --epsilon-yhat=1  --mode=full
+python3 $DIR/simulation.py --out_prefix=ccc_full_non-normal-all --delta-yc=1 --epsilon-yc=1 --delta-yhat=1 --epsilon-yhat=1  --mode=full
 
 echo "y,c,yhat: delta=1.5, epsilon=0 (kurt: 1.3 skew: 0)"
-#python3 $DIR/simulation.py --out_prefix=ccc_full_only-kurt-all --delta-yc=1.5 --epsilon-yc=0 --delta-yhat=1.5 --epsilon-yhat=0  --mode=full
+python3 $DIR/simulation.py --out_prefix=ccc_full_only-kurt-all --delta-yc=1.5 --epsilon-yc=0 --delta-yhat=1.5 --epsilon-yhat=0  --mode=full
 
 echo "normal y, c; non-normal yhat: delta=2, epsilon=2 (kurt: 10 skew: -3)"
-#python3 $DIR/simulation.py --out_prefix=ccc_full_non-normal-yhat --delta-yhat=2 --epsilon-yhat=3  --mode=full
+python3 $DIR/simulation.py --out_prefix=ccc_full_non-normal-yhat --delta-yhat=2 --epsilon-yhat=3  --mode=full
 
 ####################### binary c
 
