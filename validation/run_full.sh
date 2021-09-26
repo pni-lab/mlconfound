e2a35330986c800c84ac5192603a69c007ef9bab
Tamas Spisak <tamas.spisak@uk-essen.de>
2021-09-26 15:25:12 +0200

    GAM-based conditional distribution estimation

--- validation/run_full.sh
+++ validation/run_full.sh
@@ -6,24 +6,11 @@ DIR=`dirname $0`
 echo "normal y, yhat, c"
 python3 $DIR/simulation.py --out_prefix=ccc_full_normal --mode=full
 
-echo "non-nomral y,c,yhat: delta=1, epsilon=1 (kurt: 1.1 skew: -1.1)"
-python3 $DIR/simulation.py --out_prefix=ccc_full_non-normal-all --delta-yc=1 --epsilon-yc=1 --delta-yhat=1 --epsilon-yhat=1  --mode=full
-
-echo "non-nomral y,c,yhat: delta=1.05, epsilon=3 (kurt: 2 skew: -1.5)"
-python3 $DIR/simulation.py --out_prefix=ccc_full_non-normal-all2 --delta-yc=1.05 --epsilon-yc=3 --delta-yhat=1.05 --epsilon-yhat=3 --mode=full
-
-echo "non-nomral y,c,yhat: delta=1.5, epsilon=5 (kurt: 5 skew: -2)"
-python3 $DIR/simulation.py --out_prefix=ccc_full_non-normal-all3 --delta-yc=1.5 --epsilon-yc=5 --delta-yhat=1.5 --epsilon-yhat=5 --mode=full
-
-echo "y,c,yhat: delta=1.5, epsilon=0 (kurt: 1.3 skew: 0)"
-python3 $DIR/simulation.py --out_prefix=ccc_full_only-kurt-all --delta-yc=1.5 --epsilon-yc=0 --delta-yhat=1.5 --epsilon-yhat=0  --mode=full
-
-echo "normal y, c; non-normal yhat: delta=2, epsilon=2 (kurt: 10 skew: -3)"
-python3 $DIR/simulation.py --out_prefix=ccc_full_non-normal-yhat --delta-yhat=2 --epsilon-yhat=3  --mode=full
-
 ####################### all cont - squared relationship
-echo "normal y, yhat, c"
-python3 $DIR/simulation.py --out_prefix=ccc_full_normal --mode=full
+echo "normal y, yhat, c - squared"
+python3 $DIR/simulation.py --out_prefix=ccc_full_normal --mode=full --nonlin-trf=squared
+
+#######################
 
 echo "non-nomral y,c,yhat: delta=1, epsilon=1 (kurt: 1.1 skew: -1.1)"
 python3 $DIR/simulation.py --out_prefix=ccc_full_non-normal-all --delta-yc=1 --epsilon-yc=1 --delta-yhat=1 --epsilon-yhat=1  --mode=full
@@ -39,7 +26,6 @@ python3 $DIR/simulation.py --out_prefix=ccc_full_only-kurt-all --delta-yc=1.5 --
 
 echo "normal y, c; non-normal yhat: delta=2, epsilon=2 (kurt: 10 skew: -3)"
 python3 $DIR/simulation.py --out_prefix=ccc_full_non-normal-yhat --delta-yhat=2 --epsilon-yhat=3  --mode=full
-
 
 ####################### binary c
 
--- validation/run_partial.sh
+++ validation/run_partial.sh
@@ -7,6 +7,12 @@ DIR=`dirname $0`
 echo "normal y, yhat, c"
 python3 $DIR/simulation.py --out_prefix=ccc_partial_normal
 
+####################### all cont - squared relationship
+echo "normal y, yhat, c - squared"
+python3 $DIR/simulation.py --out_prefix=ccc_partial_normal --nonlin-trf=squared
+
+#######################
+
 echo "non-nomral y,c,yhat: delta=1, epsilon=1 (kurt: 1.1 skew: -1.1)"
 python3 $DIR/simulation.py --out_prefix=ccc_partial_non-normal-all --delta-yc=1 --epsilon-yc=1 --delta-yhat=1 --epsilon-yhat=1
 
@@ -21,7 +27,6 @@ python3 $DIR/simulation.py --out_prefix=ccc_partial_only-kurt-all --delta-yc=1.5
 
 echo "normal y, c; non-normal yhat: delta=2, epsilon=2 (kurt: 10 skew: -3)"
 python3 $DIR/simulation.py --out_prefix=ccc_partial_non-normal-yhat --delta-yhat=2 --epsilon-yhat=3
-
 
 
 ####################### binary c
